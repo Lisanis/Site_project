a483d582c1a9ca90ee6b6049f015a704989a9af4
Dzianis Kalesnikau <denkoles3k@gmail.com>
2022-06-08 23:34:00 +0300

    Final version, comments added

--- SiteGroup139/weather/views.py
+++ SiteGroup139/weather/views.py
@@ -8,6 +8,7 @@ def index(request):
     api_token = settings.MY_API_KEY
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + api_token
 
+    # Избегаем дубликаты городов с помощью get_or_create.
     if request.method == 'POST':
         city_input = request.POST.get('city_input')
         city_input = city_input.title()
@@ -16,13 +17,16 @@ def index(request):
     cities = City.objects.all()
     all_cities = []
     key_error = ''
+
+    # Получаем(обновляем) информацию по всем имеющимся на отслеживании городам,
+    # проверяем правильность ввода названия города..
     for city in cities:
         try:
             res = requests.get(url.format(city.name)).json()
             city_info = {
                 'city': city.name,
                 'temp': res["main"]["temp"],
-                'icon': res["weather"][0]["icon"]
+                'icon': res["weather"][0]["icon"],
             }
             all_cities.append(city_info)
         except KeyError:
