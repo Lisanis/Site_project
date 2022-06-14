9a8ee47b7854cc0cc9f79a93bf10e2bbd3c4fd54
Dzianis Kalesnikau <denkoles3k@gmail.com>
2022-06-14 19:10:59 +0300

    Heroku config

--- SiteGroup139/SiteGroup139/settings.py
+++ SiteGroup139/SiteGroup139/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = key.SECRET_KEY_SITE
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
